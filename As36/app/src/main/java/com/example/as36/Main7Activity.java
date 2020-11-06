1696913a298ab912fd7c2485d83cdf82e71b5670
Chengyu1012 <2696322235@qq.com>
2020-11-06 09:36:53 +0800

    提交了78题

--- As36/app/src/main/java/com/example/as36/Main7Activity.java
+++ As36/app/src/main/java/com/example/as36/Main7Activity.java
@@ -1,14 +1,37 @@
 package com.example.as36;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Main7Activity extends AppCompatActivity {
+public class Main7Activity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btn_a7_1;
+    private TextView tv_a7_1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main7);
+        initView();
+    }
+
+    private void initView() {
+        btn_a7_1 = (Button) findViewById(R.id.btn_a7_1);
+
+        btn_a7_1.setOnClickListener(this);
+        tv_a7_1 = (TextView) findViewById(R.id.tv_a7_1);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_a7_1:
+                tv_a7_1.setText("傻逼，你还真的按？");
+                break;
+        }
     }
 }
--- As36/app/src/main/java/com/example/as36/Main8Activity.java
+++ As36/app/src/main/java/com/example/as36/Main8Activity.java
@@ -1,14 +1,37 @@
 package com.example.as36;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Main8Activity extends AppCompatActivity {
+public class Main8Activity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView tv_a8_1;
+    private Button btn_a8_1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main8);
+        initView();
+    }
+
+    private void initView() {
+        tv_a8_1 = (TextView) findViewById(R.id.tv_a8_1);
+        btn_a8_1 = (Button) findViewById(R.id.btn_a8_1);
+
+        btn_a8_1.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_a8_1:
+                tv_a8_1.setText("你还真按呀");
+                break;
+        }
     }
 }
